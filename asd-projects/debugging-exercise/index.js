9bb9a8a6e0f52f80ea1735f169722a1d9aa295cc
Richard Bergeron <rbergeron2022@gmail.com>
2024-02-20 16:44:17 +0000

    debugging exercise finished

--- asd-projects/debugging-exercise/index.js
+++ asd-projects/debugging-exercise/index.js
@@ -90,7 +90,7 @@ function update(){
     // to make seeing issues in the debugger slightly easier (in practice, you should use
     // circles.length, but do NOT change it here)
     for (var i = 0; i < maxCircles; i++){
-        var circle = circles[j];
+        var circle = circles[i];
 
         // move the circle
         moveCircle(circle);
@@ -109,7 +109,7 @@ function update(){
 
 // this moves circles in memory but doesn't update them on the screen
 function moveCircle(circle){
-    circle.x = circle.speedX;
+    circle.x += circle.speedX;
     circle.y += circle.speedY;
 }
 
@@ -117,30 +117,30 @@ function moveCircle(circle){
 function bounceCircle(circle){
 
     // this bounces off the left wall
-    if (circle.x < 0){
+    if (circle.x < 7){
         circle.x -= circle.speedX;
         circle.speedX *= -1;
     }
     // this bounces off the right wall
-    else if (circle.x > boardWidth){
+    else if (circle.x > boardWidth - 7){
         circle.x -= circle.speedX;
         circle.speedX *= -1;
     }
     // this bounces off the top wall
-    if (circle.y < 0){
+    if (circle.y < 7){
         circle.y -= circle.speedY;
         circle.speedY *= -1;
     }
     // this bounces off the bottom wall
-    else if (circle.y > boardHeight){
+    else if (circle.y > boardHeight - 7){
         circle.y -= circle.speedY;
-        circle.speedX *= -1;
+        circle.speedY *= -1;
     }
 }
 
 // this redraws the circle's position on the screen
 function updateCircleOnScreen(circle){
-    maxCircles = 0;
+    
 
     // these lines redraw the circle's position
     $(circle.id).css('left', circle.x);
